72b2d6acaeb939ab99e69f34cd8958faabb811ca
Tp216 <101172504+Tp216@users.noreply.github.com>
2022-03-31 10:05:45 +0300

    Walls
    Walls

---
+++ Assets/Scipts/CarChange.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarChange : MonoBehaviour
+{
+    public GameObject Car1;
+    public GameObject Car2;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}
