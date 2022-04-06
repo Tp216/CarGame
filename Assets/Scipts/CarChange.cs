b5660de024fc04cb8613ac04cf5264c9f58bf752
Tp216 <101172504+Tp216@users.noreply.github.com>
2022-04-06 07:46:41 +0300

    car change basics

--- Assets/Scipts/CarChange.cs
+++ Assets/Scipts/CarChange.cs
@@ -6,9 +6,11 @@ public class CarChange : MonoBehaviour
 {
     public GameObject Car1;
     public GameObject Car2;
+
     private void OnTriggerEnter(Collider other)
     {
-        
-    }
+        Car1.SetActive(false);
+        Car2.SetActive(true);
 
+    }
 }
